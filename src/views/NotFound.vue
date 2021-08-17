0eb1eaf3cbe72bf1e8f8ff7e77dee09a6f5b2938
wozien <571455773@qq.com>
2021-08-18 01:33:46 +0800

    test ssr

---
+++ src/views/NotFound.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <h1>404</h1>
+    <p>The page you're looking for could not be found. Try selecting one of the lists in the nav bar.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  HTTPStatus: 404
+}
+</script>
+
+<style>
+  .container {
+    background-color: #fff;
+    border-radius: 2px;
+    position: absolute;
+    margin: 70px 0;
+    width: 100%;
+    text-align: center;
+    padding: 50px 0;
+  }
+</style>
---
+++ src/views/__tests__/NotFound.server.spec.js
@@ -0,0 +1,17 @@
+/**
+ * @jest-enviroment node
+ */
+import { renderToString, render } from '@vue/server-test-utils'
+import NotFound from '../NotFound.vue'
+
+describe('NotFound.vue', () => {
+  test('renders correctly on server', async () => {
+    const html = await renderToString(NotFound)
+    expect(html).toMatchSnapshot()
+  })
+
+  test('renders 404 inside <h1>', async () => {
+    const wrapper = await render(NotFound)
+    expect(wrapper.find('h1').text()).toBe('404')
+  })
+})
